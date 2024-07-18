e1bacbbbe49a83f2998e3a99e389a3e6833f00e8
TakshPatel <pateltaksh20061022@gmail.com>
2024-07-18 10:45:42 +0530

    final commit

--- Weather_Checker.js
+++ Weather_Checker.js
@@ -3,6 +3,7 @@ let current = document.getElementById(`current`);
 let History = document.getElementById(`History`);
 let forecast = document.getElementById(`forecast`);
 let SubmitButton = document.getElementById(`SubmitButton`);
+document.addEventListener(`keydown`,(e)=>{if(e.key === `Enter`) Validate()})
 function Validate(){
   if(place.value == "") return window.alert(`Enter Correct Place`);
   if(!current.checked && !History.checked && !forecast.checked) return window.alert(`Select One Of The Options`);
@@ -21,28 +22,45 @@ case forecast.checked:forecastWeather(); break;
 async function currentWeather(){
   let api = `https://api.weatherapi.com/v1/current.json?key=17fbee2d5e634fbe89181703241107&q=${place.value}&aqi=no`
   let data = await(fetch(api).then(res => res.json()))
-  checkingErrors(data,api)
+  checkingErrors(data)
   if(result){
   document.location.href = "current.html"}
     
 }
 async function HistoryWeather(){
- let date = new Date();
- let Tdate = date.getDay()
-
+ let dataArray = []
+ let tareek = new Date()
+ let date_issh = new Date(tareek)
+ let val=true;
+ for(let i = 1;i<7;i++){
+ date_issh.setDate(tareek.getDate() - i)
+ let api = `https://api.weatherapi.com/v1/history.json?key=17fbee2d5e634fbe89181703241107&q=${place.value}&dt=${date_issh.getFullYear()}-${date_issh.getMonth() +1}-${date_issh.getDate()}`
+ let data=await(fetch(api).then(res => res.json()))
+ checkingErrors(data,val,dataArray)
+ if(result === false) break;
+}
+localStorage.setItem(`data`,JSON.stringify(dataArray))
+if(result) document.location.href = `History.html`
 }
 async function forecastWeather(){
   let api = `https://api.weatherapi.com/v1/forecast.json?key=17fbee2d5e634fbe89181703241107&q=${place.value}&days=3&aqi=no&alerts=no`
   let data = await(fetch(api).then(res=>res.json()))
-  checkingErrors(data,api)
+  checkingErrors(data)
   if(result){
     document.location.href = "forecast.html"
-  } 
+  }
 }
 
-function checkingErrors(data,api){
-  if(data.error) return (result = false,window.alert(data.error.message))
-  localStorage.setItem(`data`,JSON.stringify(data))
-  result=true;
+function checkingErrors(data,val,dataArray){
+if(data.error) return (result = false,window.alert(data.error.message))
+if(val){
+  dataArray.push(data)
+  result = true
+}
+else{
+    localStorage.setItem(`data`,JSON.stringify(data))
+    result=true;
+  }
 
 }
+ 
